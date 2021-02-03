fea394653c145e6280f42ac44bf8a0ae691c66d3
Pang <zhi-ming.pang@atos.net>
2021-02-03 11:48:57 +0800

    setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 //entry -> output
 const path = require('path');
-//console.log(path.join(__dirname,'public'));
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
+//console.log(path.join(__dirname,'public'));
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
     output: {
         path: path.join(__dirname,'public'),
         filename:'bundle.js'
@@ -15,12 +19,25 @@ module.exports = {
             exclude: /node_modules/
         }, {
             test: /\.s?css$/,
-            use: ['style-loader', 'css-loader','sass-loader']
+            //use: ['style-loader', 'css-loader','sass-loader']
+            use : CSSExtract.extract({
+                use: [{
+                    loader :'css-loader',
+                    options: { sourceMap:true}
+                }, 
+                {
+                    loader :'sass-loader',
+                    options: { sourceMap:true}
+                }
+            ]
+            })
         }]
     },
-    devtool : 'cheap-module-eval-source-map',
+    plugins: [CSSExtract],
+    devtool : isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
         contentBase:path.join(__dirname,'public'),
         historyApiFallback: true
     }
-};
+    };
+}
